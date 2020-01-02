6f2fa4d0cec2a75168cc274cfec49334f39c7859
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2020-01-02 20:33:37 +0700

    first commit

---
+++ src/main/java/com/springboot/react/mysql/SpringbootReactMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.react.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/react/mysql/SpringbootReactMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.react.mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
